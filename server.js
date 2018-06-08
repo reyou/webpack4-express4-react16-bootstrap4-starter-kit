45ccc14af42ef268410b7e8a02e118ff96155550
reyou <aytekoz@yahoo.com>
2018-06-08 14:53:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
-app.use(express.static("./"));
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+app.use(express.static(__dirname));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(appPort, function() {
-  console.log("Web started at", "http://localhost:" + appPort);
+app.listen(appPort, function () {
+    console.log("server:", "http://localhost:" + appPort);
+    console.log("__dirname:", __dirname);
 });
